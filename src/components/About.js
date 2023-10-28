917f62468ffaf23e32043ab5f7c33eab298737e0
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-10-28 10:20:38 -0500

    Complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>And now for your daily spam. Hi, I'm John. Hi, I'm John...</p>
+        <img src={image} alt="I made this" />
+      </div>
+  );
 }
 
 export default About;
